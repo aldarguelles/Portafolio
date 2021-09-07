8bb0b308e59c6a3ada395099dd562ef7d59dccb7
aldarguelles <aldanielarguelles@gmail.com>
2021-09-07 01:08:27 -0300

    Primera subida

---
+++ correo.php
@@ -0,0 +1,21 @@
+<?php
+
+	if (isset($_POST['enviar'])) {
+		if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['mensaje'])) {
+			
+			$destinatario = "aldanielarguelles@gmail.com";
+			$nombre = $_POST['nombre'];
+			$asunto = "Mensaje del portafolio"
+			$email = $_POST['email'];
+			$mensaje = $_POST['mensaje'];
+
+			$header = "Enviado desde mi portafolio";
+			$mensajeCompleto = $mensaje "\nNombre: " . $nombre "\nCorreo: " . $email;
+
+			mail($destinatario, $asunto, $mensajeCompleto, $header);
+			echo "<script>alert('Mensaje enviado exitosamente')</script>";
+
+		}
+	}
+
+?>
