93e97d8f73c2ffa351db4b49d9f041860d97dd37
aldarguelles <aldanielarguelles@gmail.com>
2021-09-07 02:58:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -116,7 +116,23 @@
 		</form>
 	</section>
 	<?php
-		include("correo.php");
+		if (isset(&$_POST['enviar'])) {
+			if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['mensaje'])) {
+			
+				$destinatario = "aldanielarguelles@gmail.com";
+				$nombre = $_POST['nombre'];
+				$asunto = "Mensaje del portafolio";
+				$email = $_POST['email'];
+				$mensaje = $_POST['mensaje'];
+
+				$header = "Enviado desde mi portafolio";
+				$mensajeCompleto = $mensaje "\nNombre: " . $nombre "\nCorreo: " . $email;
+
+				mail($destinatario, $asunto, $mensajeCompleto, $header);
+				echo "<script>alert('Mensaje enviado exitosamente')</script>";
+			}
+}
+
 	?>
 </body>
 </html>
