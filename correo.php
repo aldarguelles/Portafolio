934098f2b067ea8857323ca44d09095f79d56da1
aldarguelles <aldanielarguelles@gmail.com>
2021-09-07 04:03:35 -0300

    Update correo.php

--- correo.php
+++ correo.php
@@ -6,9 +6,10 @@ if (isset($_POST['enviar'])) {
 		$asunto = "Mensaje del portafolio";
 		$email = $_POST['email'];
 		$mensaje = $_POST['mensaje'];
-		$header = "Enviado desde mi portafolio";
+		$headers = "MIME-Version: 1.0\r\n"; 
+		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 		$mensajeCompleto = $mensaje . "\nNombre: " . $nombre ."\nCorreo: " . $email;
-		$enviar = mail($destinatario, $asunto, $mensajeCompleto, $header);
+		$enviar = mail($destinatario, $asunto, $mensajeCompleto, $headers);
 		if ($enviar) {
 			echo "<script>alert('Mensaje enviado exitosamente')</script>";
 			echo "<script>window.location='https://young-dawn-01019.herokuapp.com/';</script>";
